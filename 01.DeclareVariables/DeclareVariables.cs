b2c4c7154a174560283fcc792c993b22e1463398
borislavv27 <jacobnelson553@gmail.com>
2016-01-03 21:05:42 +0200

    Homework-Primitive-Data-Types-Variables
    C# Basics
    
    Source code for homework for Software University

---
+++ 01.DeclareVariables/DeclareVariables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclareVariables
+{
+    class DeclareVariables
+    {
+        static void Main(string[] args)
+        {
+            ushort firstNumber = 52130;
+            sbyte secondNumber = -115;
+            int thirdNumber = 4825932;
+            sbyte fourthNumber = 97;
+            short fifthNumber = 10000;
+
+
+            Console.WriteLine
+                ("First number: {0}\nSecond number: {1}\nThird number: {2}\nFourth number: {3}\nFifth number: {4}", 
+                firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+        }
+    }
+}
---
+++ 04.VariableInHex/VariableInHex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableInHex
+{
+    class VariableInHex
+    {
+        static void Main(string[] args)
+        {
+            int number = 0xfe;
+            Console.WriteLine(number);
+        }
+    }
+}
